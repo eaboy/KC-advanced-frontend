d76f3e379dfb36d4fbc91175c606efb7b877c3ce
Eduardo <e.aboy@sinteac.es>
2017-07-19 22:06:15 +0200

    Modify gulpfile to use dist folder

--- gulpfile.js
+++ gulpfile.js
@@ -2,17 +2,18 @@ var gulp = require('gulp'); // importamos gulp
 var sass = require('gulp-sass');//importamos gulp-sass
 var browserSync = require('browser-sync').create();// importamos browser-sync
 var notify = require('gulp-notify');
+var gulpImport = require('gulp-html-import');
 
 //definimos la tarea por defecto
-gulp.task('default', function(){
+gulp.task('default', ['sass','html'], function(){
     // iniciamos el servidor de desarrollo
-    browserSync.init({server: 'src/'});
+    browserSync.init({server: 'dist/'});
 
     // observa cambios en los archivos sass y ejecuta la tarea sass
     gulp.watch(['src/scss/*.scss', 'src/scss/**/*.scss'], ['sass']);
 
     // observa cambios en los html y recarga el navegador
-    gulp.watch('src/*.html').on('change', browserSync.reload);
+    gulp.watch('src/*.html', ['html']);
 });
 
 // compilar sass
@@ -21,7 +22,14 @@ gulp.task('sass', function(){
         .pipe(sass().on('error', function(error) {
             return notify().write(error);
         })) // lo compilamos con gulp-sacc
-        .pipe(gulp.dest('src/css/')) // guardamos el resultado en la carpeta css
-        .pipe(browserSync.stream()) // recarga el css al vuelo
-        .pipe(notify('SASS compilado')); // muestra notificación en pantalla
+        .pipe(gulp.dest('dist/')) // guardamos el resultado en la carpeta css
+        .pipe(browserSync.stream()); // recarga el css al vuelo
+});
+
+// copiar e importar html
+gulp.task('html', function(){
+    gulp.src('src/*.html')
+        .pipe(gulpImport('src/components/*'))
+        .pipe(gulp.dest('dist/'))
+        .pipe(browserSync.stream());
 });
